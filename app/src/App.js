9f59ccb5a02033bad4937324d84f8ed9f3627257
LuisERivas <lucho.rivas.villacorta@gmail.com>
2020-02-07 02:34:45 -0800

    added links need to age pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/'>home</Link>
+          </li>
+          <li>
+            <Link to='/Workspace'>workspace </Link>
+          </li>
+          <li>
+            <Link to='/CreateNewPatient'>Create new patient </Link>
+          </li>
+          <li>
+            <Link to='/PatientList'>Patient List </Link>
+          </li>
+          <li>
+            <Link to='/AlphabeticSearch'>Alphabetic Search </Link>
+          </li>
+          <li>
+            <Link to='/PatientPage'>PatientPage </Link>
+          </li>
+        </ul>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
